a27aa77cbb61880d221d3df92b3345f090c0bcfd
Adeyoju Ibukunoluwa Joel <adeyojuibukunoluwa1@gmail.com>
2025-04-14 01:10:12 +0100

    Image bug fixed

--- src/app/portfolio/about/page.tsx
+++ src/app/portfolio/about/page.tsx
@@ -1,14 +1,56 @@
 import { Header } from '@/components/features/portfolio/Header'
+import { Footer } from '@/components/globals/layout/Footer'
 import { Navbar } from '@/components/globals/layout/Navbar'
 import React from 'react'
 
 const About = () => {
     return (
-        <div className=" flex flex-col justify-between px-6 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+        <div className="flex flex-col bg-gray-100 dark:bg-gray-950 text-gray-900 dark:text-gray-100 overflow-hidden">
             <Navbar type="portfolio" />
-            <div className="min-h-screen">
-                <Header/>
+
+            <div className="relative min-h-screen flex items-center px-6 md:px-16 bg-transparent">
+                
+                {/* Header Content */}
+                <div className="z-10 w-full bg-transparent">
+                    <Header />
+                </div>
+                
+                {/* Background with Low Opacity (for parent div) */}
+                <div className="absolute inset-0 bg-cover bg-center opacity-[0.03]"
+                        style={{
+                            backgroundImage: `url('/images/pattern/topography.svg')`
+                        }}
+                >
+                </div>
+                <div
+                    className="absolute inset-0 bg-cover bg-center opacity-[0.02] dark:opacity-[0.05]"
+                    style={{
+                        backgroundImage: `url('/images/pattern/topography-light.svg')`,
+                    }}
+                >
+                </div>
+
+                {/* Painted Triangle Backgrounds with Noise */}
+                <div className="absolute inset-0 z-10 bg-transparent ">
+                    {/* Top Triangle with Fade-in Animation */}
+                    <div
+                        className="absolute top-0 right-0 w-1/2 h-1/3 bg-[#EBAA4C] opacity-90 clip-triangle-top animate-fadeIn"
+                        style={{
+                            backgroundImage: "url('/images/pattern/topography.svg')",
+                            zIndex: 5,
+                        }}
+                    />
+                    {/* Bottom Triangle with Fade-in Animation */}
+                    <div
+                        className="absolute bottom-0 left-0 w-full h-1/2 bg-[#716EA4] opacity-90 clip-triangle-bottom animate-fadeIn"
+                        style={{
+                            backgroundImage: "url('/images/pattern/topography.svg')",
+                            zIndex: 5,
+                        }}
+                    />
+                </div>
             </div>
+            <Footer type='portfolio'/>
         </div>    )
 }
 

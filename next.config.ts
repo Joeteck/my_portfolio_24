7d016dff09937578adef07cdc585cd276abbec69
Adeyoju Ibukunoluwa Joel <adeyojuibukunoluwa1@gmail.com>
2025-08-26 00:26:46 +0100

    chore: remove experimental TurboPack configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {}, // disable Turbopack's special handling
-    },
+  reactStrictMode: true, // Helps catch potential issues
+  swcMinify: true,       // Uses the faster SWC compiler for minification
+  images: {
+    domains: [
+      "localhost", 
+      "res.cloudinary.com", 
+      "avatars.githubusercontent.com"
+      // add any other domains you use for <Image src="" />
+    ],
   },
 };
 

3de129d4e43adc7e66fe83b8fad61ec2651e5a93
Adeyoju Ibukunoluwa Joel <adeyojuibukunoluwa1@gmail.com>
2025-09-21 23:44:11 +0100

    chore: simplify Loader.io verification page content

--- src/app/loaderio-63301e82fd588b8e44e091e909972a1a/page.tsx
+++ src/app/loaderio-63301e82fd588b8e44e091e909972a1a/page.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 
 const LoaderioVerificationPage: React.FC = () => (
     <div>
-        <h1>Loader.io Verification</h1>
-        <p>
-            Please verify your site by adding the following meta tag to your site&apos;s <code>&lt;head&gt;</code> section:
-        </p>
-        <pre>
-            <code>
-                {`<meta name="loaderio" content="63301e82fd588b8e44e091e909972a1a" />`}
-            </code>
-        </pre>
-        <p>
-            Once you&apos;ve added the meta tag, you can proceed with Loader.io verification.
-        </p>
+        loaderio-63301e82fd588b8e44e091e909972a1a
     </div>
 );
 

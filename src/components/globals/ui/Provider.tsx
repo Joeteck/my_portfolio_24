f5414d51f9c6e3a3be0fa2fa6ef134f64f33538d
Adeyoju Joel <adeyojuibukunoluwa1@gmail.com>
2026-07-11 21:59:53 +0100

    refactor: remove mounted state logic from Providers component

--- src/components/globals/ui/Provider.tsx
+++ src/components/globals/ui/Provider.tsx
@@ -1,22 +1,12 @@
 'use client';
+
 import { ThemeProvider } from 'next-themes';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 
 export function Providers({ children }: { children: ReactNode }) {
-    const [mounted, setMounted] = useState(false);
-
-    // Ensure the component is mounted before rendering
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) {
-        return null;
-    }
-
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
+            {children}
         </ThemeProvider>
     );
 }
